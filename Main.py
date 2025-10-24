7d1f4f1347cb1b676a3910fd979fe0b3180ef374
Errne <ernrest.fraiman@gmail.com>
2025-10-24 18:57:15 +0100

    reaaranges random events

--- Main.py
+++ Main.py
@@ -1,15 +1,5 @@
-import random
 
-# import self as self
-
-from Enemy import *
-from Player import *
 from Game import *
-from Battle import *
-from Room import *
-from Shop import *
-
-
 
 game = Game()
 player = game.display_intro()
@@ -17,7 +7,7 @@ player = game.display_intro()
 if player is None:
     player = game.create_player(game.player_name)
 
-player.add_gold_to_pouch(1000)
+# player.add_gold_to_pouch(1000)
 
 game.enter_world(player)
 
